80d6d2df0c1a19a12436ace9d0c15834c58b3399
noralir <noralienroneid@gmail.com>
2023-02-15 15:13:17 +0100

    normalized on capacity

--- generic_simulator.py
+++ generic_simulator.py
@@ -4,13 +4,13 @@ import json
 import numpy as np
 import os
 
-def inter_arrival_time(dist_type_pkt_ia_time, avg_pkt_ia_time_us):
+def inter_arrival_time(dist_type_pkt_ia_time, avg_pkt_ia_time):
     if dist_type_pkt_ia_time == 'M':
-        return np.random.exponential(avg_pkt_ia_time_us)
+        return np.random.exponential(avg_pkt_ia_time)
     elif dist_type_pkt_ia_time == 'D':
-        return avg_pkt_ia_time_us
+        return avg_pkt_ia_time
     else:
-        return avg_pkt_ia_time_us # D is standard
+        return avg_pkt_ia_time # D is standard
 
 def packet_size(dist_type_pkt_len, avg_pkt_len_bits):
     if dist_type_pkt_len == 'M':
@@ -26,44 +26,44 @@ def generic_simulator(input_variables, filename_data, folder_nth):
         for i in range(len(input_variables["num_pkts"])):
             j = 0 
             while j <= input_variables["num_pkts"][i]:
-                yield env.timeout(inter_arrival_time(input_variables["dist_type_pkt_ia_time"][i], input_variables["avg_pkt_ia_time_us"][i]))
+                yield env.timeout(inter_arrival_time(input_variables["dist_type_pkt_ia_time"][i], input_variables["avg_pkt_ia_time"][i]))
                 env.process(packet(env, packet_size(input_variables["dist_type_pkt_len"][i], input_variables["avg_pkt_len_bits"][i]), j, i))
                 j += 1
             j = 0
 
     def packet(env, pkt_size_bits, number, dist_i):
-        t_generated_us = env.now
+        t_generated = env.now
         n_in_queue = len(buffer.queue)
         with buffer.request() as req:
             yield req
             t_processing_start = env.now
-            yield env.timeout(pkt_size_bits / input_variables["capacity_bps"] * 10**6)
+            yield env.timeout(pkt_size_bits / input_variables["capacity"])
             t_processing_end = env.now 
         # Calculate data
-        t_buffer_us = t_processing_start - t_generated_us
-        t_processing_us = t_processing_end - t_processing_start
+        t_buffer = t_processing_start - t_generated
+        t_processing = t_processing_end - t_processing_start
         # Save data 
-        if filename_data != False:
-            writer.writerow([number, t_generated_us, t_buffer_us, t_processing_us, pkt_size_bits, n_in_queue])
-        if number in [0, 10, 100, 1000, 10000, 100000, 1000000] and folder_nth != False:
+        if filename_data:
+            writer.writerow([number, t_generated, t_buffer, t_processing, pkt_size_bits, n_in_queue])
+        if number in [0, 10, 100, 1000, 10000, 100000, 1000000] and folder_nth:
             filename = folder_nth + str(dist_i) + '/' + str(number) +'.csv'
             exist = os.path.isfile(filename)
             with open(filename, 'a', newline='') as fileN:
                 writerN = csv.writer(fileN)
                 if not exist:
-                    writerN.writerow(["number", "t_generated_us", "t_buffer_us", "t_processing_us", "pkt_size_bits", "n_in_queue"])
-                writerN.writerow([number, t_generated_us, t_buffer_us, t_processing_us, pkt_size_bits, n_in_queue])
+                    writerN.writerow(["number", "t_generated", "t_buffer", "t_processing", "pkt_size_bits", "n_in_queue"])
+                writerN.writerow([number, t_generated, t_buffer, t_processing, pkt_size_bits, n_in_queue])
 
-    if filename_data != False:
+    if filename_data:
         f_data = open(filename_data, 'w', newline='')
         writer = csv.writer(f_data)
-        writer.writerow(["number", "t_generated_us", "t_buffer_us", "t_processing_us", "pkt_size_bits", "n_in_queue"])
+        writer.writerow(["number", "t_generated", "t_buffer", "t_processing", "pkt_size_bits", "n_in_queue"])
 
     env = simpy.Environment()
     buffer = simpy.Resource(env)
     env.process(packetgenerator(env))
     env.run()
-    if filename_data != False:
+    if filename_data:
         f_data.close()
 
 
--- verify_MM1.py
+++ verify_MM1.py
@@ -9,11 +9,11 @@ def plotsojournMM1(filename_input, input_index, filename_data):
     with open(filename_input, 'r') as f_input:
         input_variables = json.load(f_input)
     f = lambda u, l, x : (u-l) * math.e**(-(u-l)*x) #sojourn time (eq 6.85 p172 ttm4110 book)
-    lambda_ = 1/input_variables["avg_pkt_ia_time_us"][input_index]
-    mu_ = 1/((input_variables["avg_pkt_len_bits"][input_index]/input_variables["capacity_bps"])*10**6)
+    lambda_ = 1/input_variables["avg_pkt_ia_time"][input_index]
+    mu_ = 1/((input_variables["avg_pkt_len_bits"][input_index]/input_variables["capacity"]))
     plt.plot(
-        np.arange(0, 100, 0.25), 
-        f(mu_,lambda_, np.arange(0, 100, 0.25)), 
+        np.arange(0, 20000, 100), 
+        f(mu_,lambda_, np.arange(0, 20000, 100)), 
         label="Theoretical distribution", 
         color="blueviolet"
     )
@@ -22,7 +22,7 @@ def plotsojournMM1(filename_input, input_index, filename_data):
     # --- SIMULATION --------------------- #
     fields_data, rows_data = read_data_csv(filename_data)
     delay = [row[2]+row[3] for row in rows_data]
-    plt.hist(delay, density=True, label="Simulation data", color='aquamarine', bins=100)
+    plt.hist(delay, density=True, label="Simulation data", color='aquamarine', bins=200)
     # ------------------------------------ #
 
 
@@ -40,11 +40,11 @@ def plotwaitMM1(filename_input, input_index, filename_data):
     with open(filename_input, 'r') as f_input:
         input_variables = json.load(f_input)
     f = lambda u, l, x : 1 - l/u * math.e**(-(u-l)*x) #wait time (eq. 6.84 p 172 ttm4110 book)
-    lambda_ = 1/input_variables["avg_pkt_ia_time_us"][input_index]
-    mu_ = 1/((input_variables["avg_pkt_len_bits"][input_index]/input_variables["capacity_bps"])*10**6)
+    lambda_ = 1/input_variables["avg_pkt_ia_time"][input_index]
+    mu_ = 1/((input_variables["avg_pkt_len_bits"][input_index]/input_variables["capacity"]))
     plt.plot(
-        np.arange(0, 100, 0.25), 
-        f(mu_, lambda_, np.arange(0, 100, 0.25)), 
+        np.arange(0, 20000, 100), 
+        f(mu_, lambda_, np.arange(0, 20000, 100)), 
         label="Theoretical distribution", 
         color="blueviolet"
     )
@@ -53,7 +53,7 @@ def plotwaitMM1(filename_input, input_index, filename_data):
     # --- SIMULATION --------------------- #
     fields_data, rows_data = read_data_csv(filename_data)
     wait = [row[2] for row in rows_data]
-    plt.hist(wait, density=True, label="Simulation data", color='aquamarine', bins=100)
+    plt.hist(wait, density=True, label="Simulation data", color='aquamarine', bins=200)
     # ------------------------------------ #
     
     # --- PLOT --------------------------- #
@@ -65,5 +65,5 @@ def plotwaitMM1(filename_input, input_index, filename_data):
     # ------------------------------------ #
 
 
-plotsojournMM1(filename_input="05_MM1-MD1/input.json", input_index=0, filename_data="05_MM1-MD1/MM1/0.csv")
+plotsojournMM1(filename_input="06_MM1/input.json", input_index=0, filename_data="06_MM1/data/0.csv")
 #plotwaitMM1(filename_input="05_MM1-MD1/input.json", input_index=0, filename_data="05_MM1-MD1/MM1/0.csv")
