d38b1e752bfc2f265c68f568338513cb129dede1
noralir <noralienroneid@gmail.com>
2023-04-26 15:20:50 +0200

    work on DD1_10s/100s, not working fully yet

--- 22_DD1-10s.py
+++ 22_DD1-10s.py
@@ -4,27 +4,36 @@ from plot_generic import *
 from print_generic_stats import generic_stats
 from create_list_with_given_average import create_list_with_given_average
 
-avg_pkt_ia_time = create_list_with_given_average(n=100, avg=15)
+n = 10
+folder = "22_DD1-10s/"
+
+
+
+avg_pkt_ia_time = create_list_with_given_average(n=n, avg=15)
+
+avg_pkt_len_bits = [list(range(8,18))]
+avg_pkt_len_bits.reverse()
+
 
 input_variables = {
     "avg_pkt_ia_time": [avg_pkt_ia_time], 
     "sigma_squared_pkt_ia_time":[],
-    "dist_type_pkt_ia_time": [["D"]*100], 
+    "dist_type_pkt_ia_time": [["D"]*n], 
 
-    "avg_pkt_len_bits": [[10]*100], 
+    "avg_pkt_len_bits": avg_pkt_len_bits, 
     "sigma_squared_pkt_len": [],
-    "dist_type_pkt_len": [["M"]*100], 
+    "dist_type_pkt_len": [["D"]*n], 
 
     "capacity": 1, 
-    "num_pkts": [[1000]*100],
-    "num_sources" : [[1]*100]
+    "num_pkts": [[10000]*n],
+    "num_sources" : [[1]*n]
 }                                                                                                                               
 
-folder = "21_DM1-100s/"
+
 if not os.path.exists(folder):
     os.makedirs(folder)
 
-#write_input_file(input_variables, folder+"input.json")
-#run_simulator(folder=folder, input_file="input.json", runs=1, data=True, nth=False)
+write_input_file(input_variables, folder+"input.json")
+run_simulator(folder=folder, input_file="input.json", runs=1, data=True, nth=False)
 plot_multiple_sources_no_priority(filename_input=folder+"input.json", filename_data=folder+"data/0.csv", plot_type="sojourn_pdf")
 #generic_stats(filename_input=folder+"input.json", filename_data=folder+"data/0.csv")
--- create_list_with_given_average.py
+++ create_list_with_given_average.py
@@ -1,19 +1,17 @@
 def create_list_with_given_average(n,avg):
-
     average = n * avg
+    increment_number = int(average/(n/2)/2)
 
-    test1 = int(average/(n/2)/2)
-
-    n1 = int(average-test1/2)
-    n2 = int(average+test1/2)
+    n1 = int(average-increment_number/2)
+    n2 = int(average+increment_number/2)
     avg_pkt_ia_time = [n1, n2]
 
-    current_add = test1
+    current_add = increment_number
 
     while len(avg_pkt_ia_time) < n:
         avg_pkt_ia_time.append(n1 - current_add)
         avg_pkt_ia_time.append(n2 + current_add)
-        current_add += test1
+        current_add += increment_number
 
     avg_pkt_ia_time.sort()
     return avg_pkt_ia_time
